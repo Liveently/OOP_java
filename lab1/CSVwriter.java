f880b63ef63bce6d17380f1c965f1677e954249d
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-03-07 10:32:59 +0700

    Add files via upload

--- lab1/CSVwriter.java
+++ lab1/CSVwriter.java
@@ -6,10 +6,9 @@ import java.util.Map;
 
 public class CSVwriter {
     public static void print(FileWriter output, Map<String, Integer> sortedMap, int totalWords) throws IOException {
-
-
         for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
-            output.write(entry.getKey() + "," + entry.getValue() + "," + Math.ceil((double) entry.getValue() / totalWords * 100 * 100) / 100 + "%\n");
+            double ceil = Math.ceil((double) entry.getValue() / totalWords * 100 * 100);
+            output.write(entry.getKey() + "," + entry.getValue() + "," + ceil / 100 + "%\n");
         }
         output.close();
     }
--- lab1/Main.java
+++ lab1/Main.java
@@ -7,22 +7,20 @@ import java.io.InputStreamReader;
 import java.util.Map;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        if (args.length<2){
+        if (args.length < 2) {
             System.out.println("no files");
             return;
         }
 
-        try {
-            InputStreamReader input = new InputStreamReader(new FileInputStream(args[0]));
+        try (InputStreamReader input = new InputStreamReader(new FileInputStream(args[0]));
+             FileWriter output = new FileWriter(args[1], false);) {
             WordParser parser = new WordParser();
             Map<String, Integer> words = parser.parseFile(input);
             Map<String, Integer> sortedWords = MapSorter.byValue(words);
-            FileWriter output = new FileWriter(args[1], false);
             CSVwriter.print(output, sortedWords, parser.getWordsCounter());
-        }
-        catch (IOException err) {
+        } catch (IOException err) {
             err.printStackTrace();
         }
     }
