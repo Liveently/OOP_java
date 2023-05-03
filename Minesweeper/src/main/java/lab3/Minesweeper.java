5b6e8fcc4c434351b5632ab7fa1ebb88063f7128
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-05-04 05:23:16 +0700

    Delete Minesweeper/src/main directory

--- Minesweeper/src/main/java/lab3/Minesweeper.java
+++
@@ -1,9 +0,0 @@
-package lab3;
-
-public class Minesweeper {
-    public Minesweeper(Factory factory) {
-        Model model = new Model();
-        View view = factory.createView(model);
-        Controller controller = factory.createController(model, view);
-    }
-}
