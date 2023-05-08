a89b06a76dda6cf22eb70cc2b36cc90ea801f340
Marina Kosolap <70973716+Liveently@users.noreply.github.com>
2023-05-08 20:54:15 +0700

    Add files via upload

---
+++ lab3/java/lab3/Main.java
@@ -0,0 +1,23 @@
+package lab3;
+
+import lab3.gui.GraphicalFactory;
+import lab3.text.ConsoleFactory;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Factory factory;
+
+        if (args[0].equals("Console"))
+            factory = new ConsoleFactory();
+        else
+            factory = new GraphicalFactory();
+
+        Minesweeper minesweeper = new Minesweeper(factory);
+
+    }
+
+
+}
